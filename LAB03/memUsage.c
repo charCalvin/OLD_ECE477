9be9f0e761938883030049b9cc149166af35cf2b
caskaaa <caskaaa@gmail.com>
2019-02-21 10:22:57 -0500

    First commit

---
+++ LAB03/memUsage.c
@@ -0,0 +1,45 @@
+/*
+ *	calvin skinner
+ *	Feb 20, 2019
+ *	477 Lab 3
+ *	memory usage in percent
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int memPercent();
+
+int main(int argc, char *argv){
+	printf("Memory used(%): %d\n", memPercent());
+	
+	return 0;
+}
+
+int memPercent(){
+	FILE	*fp;
+	int	memTotal = 0;
+	int	memFree = 0;
+	int	memUsed = 0;
+	int	percent = 0;
+	char	temp[32];	// capturing data we don't need
+	//double	tempD;	// for testing
+
+
+	// open file and retreive data
+	fp = fopen("/proc/meminfo", "r");
+
+	fscanf(fp, "%*s %d %*s\n%*s %d", &memTotal, &memFree);
+
+	fclose(fp);	// close file
+
+	// caculate memory usage:
+	memUsed = memTotal - memFree;	// calculate memUsed
+	percent = ((double)memUsed / memTotal) * 100;
+	
+	// for testing
+	//printf("memUsed / memTotal = %f\n", (100 * ((double)memUsed/memTotal)));
+
+	return percent;
+}
